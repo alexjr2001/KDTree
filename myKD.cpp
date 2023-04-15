d260a876d66a74e381ee04ca7e8c83a8404598e0
alexjr2001 <63054183+alexjr2001@users.noreply.github.com>
2023-04-15 14:51:23 -0500

    Update comments

--- myKD.cpp
+++ myKD.cpp
@@ -1,12 +1,18 @@
 /*
-PROYECTO: KD-TREE
-AUTOR: ALEXANDER SEBASTI¡N G”MEZ DEL CARPIO
-FECHA: 28/04/2022
+
+PROJECT: KD-TREE
+AUTHOR: ALEXANDER SEBASTI√ÅN G√ìMEZ DEL CARPIO
+DATE: 04/28/2022
+
+[EN]
+In this project we can see a simple implementation of the KD-Tree with templates for the data type and for the number of dimensions, it makes use
+of a "Node" structure and another one of the KDTree with their respective functions. We also find global functions of a structure like sortV() that sorts
+a vector and armarTree() that shapes the tree with initial values.
 
 [ES]
-En el presente cÛdigo podemos ver un implementaciÛn simple del KD-Tree con templates para el tipo de dato y para la cantidad de dimensiones, este hace uso
-de una estructura "Node" y otra del mismo KDTree con sus respectivas funciones. Adem·s encontramos funciones libres de una estructura como sortV() que ordena 
-un vector y armarTree() que con valores de inicio se da forma al ·rbol.
+En el presente c√≥digo podemos ver un implementaci√≥n simple del KD-Tree con templates para el tipo de dato y para la cantidad de dimensiones, este hace uso
+de una estructura "Node" y otra del mismo KDTree con sus respectivas funciones. Adem√°s encontramos funciones libres de una estructura como sortV() que ordena 
+un vector y armarTree() que con valores de inicio se da forma al √°rbol.
 */
 
 #include<iostream>
@@ -18,22 +24,22 @@ un vector y armarTree() que con valores de inicio se da forma al ·rbol.
 using namespace std;
 
 template<class T, int dim>
-struct Node {			//Estructura nodo
-	Node<T, dim>* right = NULL;			//Punteros a los costados
+struct Node {			//Node structure
+	Node<T, dim>* right = NULL;			//Pointers to the sides
 	Node<T, dim>* left = NULL;
 	vector<T> val;
-	int nivel = 0;			//Nivel en el que se encuentra del ·rbol
+	int nivel = 0;			//Its level in the tree
 	Node() {}
 	~Node() {}
 };
 
-template<class T, int dim>		//Template T del tipo de dato y dim del numero de dimensiones
+template<class T, int dim>		//Template T: Datatype and dim: Number of dimensions
 struct KDTree {				
 	Node<T, dim>* root = NULL;
 	KDTree() {}
 	~KDTree() {}
 
-	void DFS(Node<T, dim>*& ptr) {		//Sirve para imprimir el ·rbol en cualquier dimensiÛn
+	void DFS(Node<T, dim>*& ptr) {		//Prints the tree in any dimension
 		if (ptr == nullptr) {
 			return;
 		}
@@ -42,7 +48,7 @@ struct KDTree {
 		DFS(ptr->left);
 	}
 
-	double distance(Node<T,dim>* a,vector<T> b) {		//Para hallar la distancia entre un nodo y una coordenada.
+	double distance(Node<T,dim>* a,vector<T> b) {		//Discover the distance between the node and coordinates
 		double dist = 0;
 		double suma = 0;
 		for (int i = 0; i < dim; i++)
@@ -53,12 +59,12 @@ struct KDTree {
 		return dist;
 	}
 
-	void rangeQuery2(vector<T> query_i,vector<T> query_f,Node<T,dim>* ptr, vector<Node<T,dim>*> &respuesta) {		//FunciÛn recursiva para hallar todos los puntos del ·rbol dentro de un rango.
+	void rangeQuery2(vector<T> query_i,vector<T> query_f,Node<T,dim>* ptr, vector<Node<T,dim>*> &respuesta) {		//Recursive function to find put all the tree points inside a range
 		if (ptr) {
 			int discriminador = ptr->nivel % dim;
-			if (query_i[discriminador] <= ptr->val[discriminador] && ptr->val[discriminador] <= query_f[discriminador]) {		//Si la coordenada se encuentra dentro del rango seguimos buscando para la izquierda y derecha
+			if (query_i[discriminador] <= ptr->val[discriminador] && ptr->val[discriminador] <= query_f[discriminador]) {		//If the coordinate is inside the range, we keep searching to the left as well as to the right
 				bool contiene = true;
-				for (int i = 0; contiene && i < dim; i++) {							//Comprobar si todas sus dimensiones est·n dentro del rango
+				for (int i = 0; contiene && i < dim; i++) {							//Check if all the dimensions are inside the range
 					if (query_i[i] > ptr->val[i] || ptr->val[i] > query_f[i]) {
 						contiene = false;
 					}
@@ -69,7 +75,7 @@ struct KDTree {
 				rangeQuery2(query_i, query_f, ptr->left, respuesta);
 				rangeQuery2(query_i, query_f, ptr->right, respuesta);
 			}
-			else if (query_i[discriminador] > ptr->val[discriminador]) {		//Si no est· en el rango buscamos un punto que si lo estÈ
+			else if (query_i[discriminador] > ptr->val[discriminador]) {		//If it's not in the range, we look for a point that is
 				rangeQuery2(query_i, query_f, ptr->right, respuesta);
 			}
 			else {
@@ -79,18 +85,18 @@ struct KDTree {
 		return;
 	}
 
-	void rangeQuery() {		//Su funciÛn es obtener el rango de b˙squeda y luego llamar a rangeQuery2() para devolver los puntos en ese rango.
+	void rangeQuery() {		//It gets the search range and then it calls 'rangeQuery2()' in order to return the points in that interval.
 		vector<T> query_i;
 		vector<T> query_f;
 		vector<Node<T,dim>*> respuesta;
 		T v1,v2;
-		cout << "\nIngresar intervalos (de menor a mayor) de busqueda\n";
+		cout << "\nIngresar intervalos (de menor a mayor) de busqueda\n";  //COUT the interval
 		for (int i = 0; i < dim; i++)
 		{
 			cout << "\tPara dimension "<< i << ": ";
 			cin >> v1 >> v2;
-			query_i.push_back(v1); //v1 es el menor
-			query_f.push_back(v2); //v2 es mayor
+			query_i.push_back(v1); //Lower limit
+			query_f.push_back(v2); //Upper limit
 		}
 		rangeQuery2(query_i,query_f,root,respuesta);
 		cout << "Los nodos en ese rango son: ";
@@ -102,18 +108,18 @@ struct KDTree {
 		}
 	}
 
-	void NN2(vector<T> pto,Node<T,dim>* now,Node<T,dim>*& candidate, int depth, double& bestD=-1) {			//Encuentra el punto m·s cercano al punto ingresado en NN con recursividad.
+	void NN2(vector<T> pto,Node<T,dim>* now,Node<T,dim>*& candidate, int depth, double& bestD=-1) {			//Find the closest point to the point entered in NN with recursion.
 		if (!now) {
 			return;
 		}
-		if (bestD==-1 || distance(now, pto) < bestD) {			//Si la distancia actual es mejor de la ya guardada la reemplazamos
+		if (bestD==-1 || distance(now, pto) < bestD) {			//If the current distance is better than the one already saved, we replace it
 			bestD = distance(now, pto);
 			candidate = now;
 		}
 
 		int axis = depth % dim;
 		bool right = false;
-		if (pto[axis] < now->val[axis]) {				//Vemos si el punto est· al lado derecho o izquierdo
+		if (pto[axis] < now->val[axis]) {				//We see if the point is on the right or left side
 			right = false;
 			NN2(pto,now->left,candidate,depth++,bestD);
 		}
@@ -121,7 +127,7 @@ struct KDTree {
 			right = true;
 			NN2(pto,now->right, candidate, depth++, bestD);
 		}
-		if (fabs(now->val[axis] - pto[axis]) < candidate->val[axis]) {			//Si la distancia actual haciendo una circunferencia supera el lado a ignorar
+		if (fabs(now->val[axis] - pto[axis]) < candidate->val[axis]) {			//If the current distance making a circle exceeds the side to be ignored
 			if (right) {
 				NN2(pto,now->left, candidate, depth++, bestD);
 			}
@@ -131,7 +137,7 @@ struct KDTree {
 		}
 	}
 
-	void NN() {		//Obtiene un punto no existente en el ·rbol y llama a NN().
+	void NN() {		//Gets a non-existent point in the tree and calls NN().
 		vector<T> query;
 		T v;
 		cout << "\nIngrese las coordenadas para buscar su NN: ";
@@ -151,7 +157,7 @@ struct KDTree {
 
 		return;
 	}
-	void KNN2(vector<T> query, vector<pair<double,Node<T, dim>*>> &ans, Node<T, dim>* ptr) {		// Recursivamente recorre el ·rbol y halla los K puntos m·s cercanos al punto ingresado en KNN.
+	void KNN2(vector<T> query, vector<pair<double,Node<T, dim>*>> &ans, Node<T, dim>* ptr) {		// It recursively traverses the tree and finds the K closest points to the point entered in KNN.
 		if (!ptr) {
 			return;
 		}
@@ -160,7 +166,7 @@ struct KDTree {
 		KNN2(query,ans,ptr->left);
 	}
 
-	void KNN() {		// Obtiene un punto no existente en el ·rbol y llama a KNN().
+	void KNN() {		// Gets a non-existent point in the tree and calls KNN().
 		vector<T> query;
 		vector<pair<double,Node<T,dim>*>> ans;
 		T v;
@@ -176,7 +182,7 @@ struct KDTree {
 
 		KNN2(query, ans, root);
 
-		sort(ans.begin(),ans.end());		//Ordena todas las distancias al punto ingresado
+		sort(ans.begin(),ans.end());		//Sorts all distances to the entered point
 		
 		for (int i = 0; i < ans.size() && i < K; i++) {
 			cout << "\n\t";
@@ -188,7 +194,7 @@ struct KDTree {
 		return;
 	}
 
-	Node<T, dim>* search(Node<T, dim>* root2, Node<T, dim>*& preptr, vector<T>& query, int n) {			//Busca un nodo en el ·rbol
+	Node<T, dim>* search(Node<T, dim>* root2, Node<T, dim>*& preptr, vector<T>& query, int n) {			//Search a node in the tree
 		Node<T, dim>* resultado = nullptr;
 
 		if (query.size() == 0) {
@@ -205,23 +211,23 @@ struct KDTree {
 		Node<T, dim>* tmp;   
 
 		while (ptr) {
-			if (ptr->val[nivel % dim] > query[nivel % dim]) {   // A la izquierda si es menor el valor a buscar
+			if (ptr->val[nivel % dim] > query[nivel % dim]) {   // To the left if the value to look for is less
 				preptr = ptr;
 				ptr = ptr->left;
 			}
-			else if (ptr->val[nivel % dim] < query[nivel % dim]) {   //A la derecha si es mayor
+			else if (ptr->val[nivel % dim] < query[nivel % dim]) {   //To the right if it is greater
 				preptr = ptr;
 				ptr = ptr->right;
 			}
-			else {						//Si son iguales 
+			else {						//if they are the same
 				resultado = ptr;
-				for (int i = 0; resultado && i < dim; i++) {		//Vemos si es el punto que se est· buscando
+				for (int i = 0; resultado && i < dim; i++) {		//We see if it is the point that is being sought
 					if (ptr->val[i] != query[i]) {
 						resultado = nullptr;
 					}
 				}
 				if (resultado) return resultado;
-				else {												//Si no lo es vamos hacia los dos lados
+				else {												//If it is not, we go both ways.
 					tmp = ptr;
 					resultado = search(ptr->left, tmp, query, nivel + 1);
 					if (resultado) {
@@ -240,7 +246,7 @@ struct KDTree {
 		return resultado;
 	}
 
-	bool insert() {				//Inserta un nodo en el ·rbol
+	bool insert() {				//Inserts a node in the tree
 		vector<T> query;
 		cout << "\nInserta las nuevas coordenadas: ";
 		T v;
@@ -248,8 +254,8 @@ struct KDTree {
 			cin >> v;
 			query.push_back(v);
 		}
-		Node<T, dim>* preptr = root;						//Apuntar· al nodo un nivel arriba del lugar de inserciÛn del nuevo nodo
-		Node<T, dim>* ptr = search(root, preptr, query, 0);			//Si el nodo ya est· en el ·rbol ya no lo insertamos
+		Node<T, dim>* preptr = root;						//It will point to the node one level above the insertion location of the new node
+		Node<T, dim>* ptr = search(root, preptr, query, 0);			//If the node is already in the tree we no longer insert it
 
 		Node<T, dim>* nuevo = new Node<T, dim>;
 		if (ptr) {
@@ -258,12 +264,12 @@ struct KDTree {
 		}
 		else {
 			nuevo->val = query;
-			if (!preptr) {				//Si el ·rbol est· vacÌo
+			if (!preptr) {				//If the tree is empty
 				root = nuevo;
 			}
 			else {
 				nuevo->nivel = preptr->nivel + 1;
-				if (query[preptr->nivel % dim] > preptr->val[preptr->nivel % dim]) {		//Vemos si lo insertamos a la derecha o a la izquierda
+				if (query[preptr->nivel % dim] > preptr->val[preptr->nivel % dim]) {		//We see if we insert it to the right or to the left
 					preptr->right = nuevo;
 				}
 				else {
@@ -276,7 +282,7 @@ struct KDTree {
 
 
 
-	void findMin(Node<T, dim>*& min, Node<T, dim>* tmp, int d, Node<T, dim>*& preptr, Node<T, dim>* pretmp) {			//Encuentra el mÌnimo valor en una dimension especÌfica en un sub·rbol
+	void findMin(Node<T, dim>*& min, Node<T, dim>* tmp, int d, Node<T, dim>*& preptr, Node<T, dim>* pretmp) {			//Find the minimum value in a specific dimension in a subtree
 		if (!tmp) {
 			return;
 		}
@@ -289,16 +295,16 @@ struct KDTree {
 
 	}
 
-	void borrar2(Node<T, dim>* ptr, Node<T, dim>* preptr, Node<T, dim>* min = nullptr) {	//FunciÛn recursiva para borrar un nodo
-		if (ptr) {	//Es que se encontrÛ
-			if (ptr->right) {				//Si no es hoja y tiene hijo a la derecha
+	void borrar2(Node<T, dim>* ptr, Node<T, dim>* preptr, Node<T, dim>* min = nullptr) {	//Recursive function to delete a node
+		if (ptr) {	//It is found
+			if (ptr->right) {				//If it is not a leaf and it has a child on the right
 				min = ptr->right;
 				preptr = ptr;
-				findMin(min, ptr->right, ptr->nivel % dim, preptr, ptr);		//Debemos encontra el mÌnimo en la dimensiÛn discriminante y en su sub·rbol
+				findMin(min, ptr->right, ptr->nivel % dim, preptr, ptr);		//We must find the minimum in the discriminant dimension and in its subtree
 				ptr->val = min->val;
 				borrar2(min, preptr);
 			}
-			else if (ptr->left) {			//Tiene hijo a la izquierda que luego ser·n los hijos a la derecha
+			else if (ptr->left) {			//He has a child on the left who will later be the children on the right
 				min = ptr->left;
 				preptr = ptr;
 				findMin(min, ptr->left, ptr->nivel % dim, preptr, ptr);
@@ -307,7 +313,7 @@ struct KDTree {
 				ptr->left = nullptr;
 				borrar2(min, preptr);
 			}
-			else {							//Si es hoja solo se elimina
+			else {							//If it is a leaf, it is only deleted
 				if (preptr->right == ptr) {
 					preptr->right = nullptr;
 				}
@@ -318,7 +324,7 @@ struct KDTree {
 		}
 		return;
 	}
-	bool borrar() {				//Obtiene el valor a borrar
+	bool borrar() {				//Gets the value to delete
 		vector<T> query;
 		cout << "\nInserta las coordenadas del nodo a borrar: ";
 		T v;
@@ -338,7 +344,7 @@ struct KDTree {
 		return false;
 	}
 
-	void update() {			//Actualiza un valor en el ·rbol
+	void update() {			//Updates a value in the tree
 		vector<T> nuevas;
 		vector<T> query;
 		if (borrar()) {
@@ -350,9 +356,9 @@ struct KDTree {
 
 
 template<class T, int dim>
-struct compare						//Ayuda a la funciÛn sort de un vector a comparar para poder ordenarlo
+struct compare						//Helps the sort function of a vector to compare and order it
 {
-	int d = 0; //Dimension a comparar
+	int d = 0; //Dimension to compare
 	compare(int _d) :d(_d) {}
 	inline bool operator() (const Node<T, dim>* struct1, const Node<T, dim>* struct2)
 	{
@@ -362,23 +368,23 @@ struct compare						//Ayuda a la funciÛn sort de un vector a comparar para poder
 
 
 template<class T, int dim>
-void sortV(vector<Node<T, dim>*>& v, int d) {   //Ordena en una dimension
+void sortV(vector<Node<T, dim>*>& v, int d) {   //Sorts in a specific dimension
 	std::sort(v.begin(), v.end(), compare<T, dim>(d));
 	return;
 }
 
 template<class T, int dim>
-void armarTree(KDTree<T, dim>& KD, vector<Node<T, dim>*>& v, Node<T, dim>*& ptr, int d) {		//Con datos ingresados al inicio se arma el ·rbol de inicio
+void armarTree(KDTree<T, dim>& KD, vector<Node<T, dim>*>& v, Node<T, dim>*& ptr, int d) {		//With data entered at the start, the start tree is built
 	int size = v.size();
 	int i_nodo = size / 2;
 	if (size == 0) {
 		return;
 	}
-	sortV(v, d % dim);		//Ordena el vector de nodos en la dimensiÛn correspondiente
+	sortV(v, d % dim);		//Order the vector of nodes in the corresponding dimension
 	ptr = v[i_nodo];
 	v[i_nodo]->nivel = d;
 	vector<Node<T, dim>*> v_left;
-	copy(v.begin(), v.begin() + i_nodo, back_inserter(v_left));		//Cortamos el vector en dos para llamar armarTree() para su respectiva construcciÛn
+	copy(v.begin(), v.begin() + i_nodo, back_inserter(v_left));		//We cut the vector in two to call armarTree() for its respective construction
 	vector<Node<T, dim>*> v_right;
 	copy(v.begin() + i_nodo + 1, v.end(), back_inserter(v_right));
 
@@ -390,13 +396,13 @@ void armarTree(KDTree<T, dim>& KD, vector<Node<T, dim>*>& v, Node<T, dim>*& ptr,
 
 
 int main() {
-	const int n_nodos = 8;	//Nro de nodos
+	const int n_nodos = 8;	//No of nodes
 	const int d = 2;	//dimension
-	vector<Node<int, d>*> v_inicio;			//Almacena los nodos del inicio
-	Node<int, d>* nodos = new Node<int, d>[n_nodos]; //Creando nodos de inicio
+	vector<Node<int, d>*> v_inicio;			//Stores the startup nodes
+	Node<int, d>* nodos = new Node<int, d>[n_nodos]; //Creating startup nodes
 	KDTree<int, d> KDT;
 
-	//INSTANCIA
+	//INSTANCE
 	int instancia[n_nodos][d] = {{7,7},{1,2},{3,4},{6,9},{9,7},{18,10},{7,2},{1,10}};
 	for (int i = 0; i < n_nodos; i++) {
 		v_inicio.push_back(&nodos[i]);
@@ -405,10 +411,10 @@ int main() {
 		}
 	}
 
-	//BUCLES PARA PEDIR VALORES AL USUARIO EN EL TERMINAL//////////////////////////////////////////////
+	//LOOPS TO REQUEST VALUES FROM THE USER IN THE TERMINAL//////////////////////////////////////////////
 	/*int v;
 	for (int i = 0; i < n_nodos; i++) {
-		v_inicio.push_back(&nodos[i]);	   //Rellenando vector con coordenadas iniciales
+		v_inicio.push_back(&nodos[i]);	   //Filling vector with initial coordinates
 		for (int j = 0; j < d; j++) {		
 			cin >> v;
 			v_inicio[i]->val.push_back(v);
@@ -416,31 +422,31 @@ int main() {
 	}*/
 	//////////////////////////////////////////////////////////////////////////////////////
 
-	armarTree(KDT, v_inicio, KDT.root, 0);		//Armamos el ·rbol
+	armarTree(KDT, v_inicio, KDT.root, 0);		//we put together the tree
 
-	KDT.DFS(KDT.root);			//Imprimimos el ·rbol inicial
+	KDT.DFS(KDT.root);			//We print the initial tree
 
-	int tests = 1;			//N˙meros de tests
-	for(int i=0;i<tests ;i++){	//DESCOMENTAR PARA PROBAR CADA FUNCION----------------
+	int tests = 1;			//Number of tests
+	for(int i=0;i<tests ;i++){	//UNCOMMENT TO TEST EACH FUNCTION----------------
 
-		KDT.KNN();				//Los vecinos m·s cercanos	
+		KDT.KNN();				//The K nearest neighbors	
 
-		//KDT.NN();						//El vecino m·s cercano
+		//KDT.NN();						//the nearest neighbor
 		
 		
-		//KDT.rangeQuery();				 //Los puntos dentro de un rango
+		//KDT.rangeQuery();				 //Points within a range
 		
 
-		/*KDT.insert();					//Borrar e insertar tienen la funciÛn buscar dentro
-		KDT.DFS(KDT.root);			//Imprimimos luego de modificaciones
+		/*KDT.insert();					//Delete and insert functions have the search function inside
+		KDT.DFS(KDT.root);			//We print after modifications
 		*/
 
 		/*KDT.borrar();
-		KDT.DFS(KDT.root);			//Imprimimos luego de modificaciones
+		KDT.DFS(KDT.root);			//We print after modifications
 		*/
 
 		/*KDT.update();
-		KDT.DFS(KDT.root);			//Imprimimos luego de modificaciones
+		KDT.DFS(KDT.root);			//We print after modifications
 		*/
 
 	}
